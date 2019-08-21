508aadc2f532da5caa39732b5a8b967585060c73
Rinor Ajeti <r4ajeti@gmail.com>
2019-08-21 02:00:34 +0200

    MOVED: main.rb moved to bin/main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,6 @@
-require './SymbolTB'
-require './Functions'
-require './Board'
+require './../SymbolTB'
+require './../Functions'
+require './../Board'
 
 $debug_on = false
 $player_name = Array[nil,nil]
